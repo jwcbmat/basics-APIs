5663817606204992cbacd2fbff8e3fd84ca6b4f5
jwcbmat <johnathan.wcb@gmail.com>
2022-03-17 23:15:53 -0300

    dependencies

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
+const randomUUID = require('crypto')
+const Database = new Map()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.post('/account', function (request, response) {
+    const { cpf, name } = request.body
+    const id = randomUUID()
+
+    Database.push( {
+        cpf,
+        name,
+        id,
+        statement: []
+    })
+
+    return response.status(201).send()
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
